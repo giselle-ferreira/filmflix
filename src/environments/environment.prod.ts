58fcf873ed7a310f144998ef9e4c0ce10bf42098
giselle-fs <giselle.fs@gmail.com>
2022-01-17 20:25:50 -0300

    Initial Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'filmflix-103a6',
+    appId: '1:452661040166:web:702df4f0f674e6f29a02e7',
+    storageBucket: 'filmflix-103a6.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDDPAnpIrJPqJNCgtoWztU4HVAv_nEf_gI',
+    authDomain: 'filmflix-103a6.firebaseapp.com',
+    messagingSenderId: '452661040166',
+  },
   production: true
 };
